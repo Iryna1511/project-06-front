32586b47014040e2325260e685dd6788659a98dc
Iryna1511 <148438865+Iryna1511@users.noreply.github.com>
2024-08-28 16:39:47 +0300

    update

--- src/components/ErrorMsg/ErrorMsg.jsx
+++ src/components/ErrorMsg/ErrorMsg.jsx
@@ -2,6 +2,8 @@ import css from "./ErrorMsg.module.css";
 
 export default function ErrorMsg() {
   return (
-    <p className={css.text}>Sorry, something went wrong... Please try again!</p>
+    <p className={css.text}>
+      Sorry, something went wrong... Please, try again!
+    </p>
   );
 }
