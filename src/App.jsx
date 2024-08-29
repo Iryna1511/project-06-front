07bc162ca867c20a8d1d9ed3b4389d0746ebb5eb
Iryna1511 <148438865+Iryna1511@users.noreply.github.com>
2024-08-29 22:29:24 +0300

    fix comma and else

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,12 @@ import Header from '../src/components/Header/Header.jsx';
 import WelcomePage from "./pages/WelcomePage/WelcomePage";
 
 function App() {
-  return (
-    <div>
-      <Header />
-      <WelcomePage />;
-    </div>
-  );
+    return (
+        <div>
+            <Header />
+            <WelcomePage />;
+        </div>
+    );
+}
 
 export default App;
