63dec19853df846420c788a57d662f374815bfea
Lizabalova <yelyzavetabalova@gmail.com>
2024-08-31 20:20:27 +0200

    delete

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,11 @@
 import "./App.css";
 import Header from '../src/components/Header/Header.jsx';
 import WelcomePage from "./pages/WelcomePage/WelcomePage";
-import Logout from "./components/LogOut/Logout.jsx";
 
 function App() {
     return (
         <div>
             <Header />
-            <Logout />
             <WelcomePage />        
         </div>
     );
