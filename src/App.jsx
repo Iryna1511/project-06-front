f0df4c8b414b8817c9e35c80c5b041fff14a7f93
Iryna1511 <148438865+Iryna1511@users.noreply.github.com>
2024-08-31 21:29:11 +0300

    up

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,12 @@
 import "./App.css";
 import Header from "../src/components/Header/Header.jsx";
 import WelcomePage from "./pages/WelcomePage/WelcomePage";
-import Logout from "./components/LogOut/Logout.jsx";
 
 function App() {
   return (
     <div>
       <Header />
-
-      <WelcomePage />
-      <Logout />
+      <WelcomePage />;
     </div>
   );
 }
