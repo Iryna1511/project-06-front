360e02a13599cf94c82157938e3db1cd1a60d4e6
LytovchenkoOlenka <131744554+LytovchenkoOlenka@users.noreply.github.com>
2024-09-01 19:26:41 +0200

    fixed_roating

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,5 +1,9 @@
 import css from "./HomePage.module.css";
 
 export default function HomePage() {
-  return <div className={css.pageContainer}></div>;
+  return (
+    <div className={css.pageContainer}>
+      <h1>ТУТ СТОРІНКА ЗАРЕЄСТРОВАНОГО КОРИСТУВАЧА</h1>
+    </div>
+  );
 }
