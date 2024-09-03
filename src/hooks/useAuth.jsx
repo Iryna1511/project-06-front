20e37b863dc56144ecaa0bb3755692719c3e8960
Valeriia Tyran <kuharets.v@gmail.com>
2024-09-03 19:13:57 +0300

    hook_Auth_original

--- src/hooks/useAuth.jsx
+++ src/hooks/useAuth.jsx
@@ -1,4 +1,4 @@
-// export function useAuth() {
-//   // const token = localStorage.getItem("token");
-//   return true;
-// }
+export function useAuth() {
+  // const token = localStorage.getItem("token");
+  return true;
+}
