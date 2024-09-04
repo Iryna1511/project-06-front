230fa1763f35e39da031f0f9505656ac49b173a2
Iryna1511 <148438865+Iryna1511@users.noreply.github.com>
2024-09-04 20:46:57 +0300

    фон
    почала роботу, але ще в процесі

--- src/components/SharedLayout/SharedLayout.jsx
+++ src/components/SharedLayout/SharedLayout.jsx
@@ -4,11 +4,21 @@ import { Outlet } from "react-router-dom";
 
 export default function SharedLayout() {
   return (
-    <div className={css.container}>
-      <Header />
-      <main className={css.mainContainer}>
-        <Outlet />
-      </main>
+    <div className={css.background}>
+      <div className={css.container}>
+        <Header />
+        <main>
+          <Outlet />
+        </main>
+      </div>
     </div>
   );
 }
+
+// useEffect(() => {
+//   if (location.pathname === "/signin") {
+//     setAction("Sign In");
+//   } else if (location.pathname === "/signup") {
+//     setAction("Sign Up");
+//   }
+// }, [location.pathname]);
--- src/pages/WelcomePage/WelcomePage.jsx
+++ src/pages/WelcomePage/WelcomePage.jsx
@@ -5,11 +5,12 @@ import WhyDrinkWater from "../../components/WhyDrinkWater/WhyDrinkWater";
 //тут має бути фон і контейнер для блоків(два компоненти )
 export default function WelcomePage() {
   return (
-    <div className={css.pageContainer}>
-      <div className={css.infoContainer}>
-        <WaterConsumptionTracker />
-        <WhyDrinkWater />
-      </div>
+    // <div className={css.pageContainer}>
+    <div className={css.infoContainer}>
+      <WaterConsumptionTracker />
+      <WhyDrinkWater />
     </div>
   );
+  // {/* </div> */}
+  // );
 }
