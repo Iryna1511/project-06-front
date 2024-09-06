aca6ae5cd50d7b8dabfa96c51f35307967dff31f
Iryna1511 <148438865+Iryna1511@users.noreply.github.com>
2024-09-06 18:15:14 +0300

    стилі і рендер на Хоум

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,11 +1,13 @@
 import css from "./HomePage.module.css";
+import TodayWaterList from "../../components/TodayWaterList/TodayWaterList";
+
 // import Calendar from "../../components/Calendar/Calendar";
 
 export default function HomePage() {
   return (
-    <div className={css.pageContainer}>
-      {/* <Calendar /> */}
-      <p>тут буде контент для залогіненого користувача</p>
+    <div className={css.tables}>
+      <TodayWaterList />
+      <div className={css.month}>Month stats table</div>
     </div>
   );
 }
--- src/pages/WelcomePage/WelcomePage.jsx
+++ src/pages/WelcomePage/WelcomePage.jsx
@@ -1,16 +1,5 @@
-// import Main from "../../components/Main/Main";
-import css from "./WelcomePage.module.css";
-import TodayWaterList from "../../components/TodayWaterList/TodayWaterList";
-
-// export default function WelcomePage() {
-//   return <Main />;
-// }
+import Main from "../../components/Main/Main";
 
 export default function WelcomePage() {
-  return (
-    <div className={css.tables}>
-      <TodayWaterList />
-      <div className={css.month}>Month stats table</div>
-    </div>
-  );
+  return <Main />;
 }
