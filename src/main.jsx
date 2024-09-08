b7992710a3a26f930b70232a55e0d25fbddc503e
Iryna1511 <148438865+Iryna1511@users.noreply.github.com>
2024-09-08 23:57:11 +0300

    up

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,14 @@
-import { StrictMode } from "react";
-import { BrowserRouter } from "react-router-dom";
-import App from "./App.jsx";
-import { PersistGate } from "redux-persist/integration/react";
-import ReactDOM from "react-dom/client";
 import "modern-normalize";
 import "./index.css";
+
+import ReactDOM from "react-dom/client";
+import { StrictMode } from "react";
 import { Provider } from "react-redux";
+import { PersistGate } from "redux-persist/integration/react";
+import { BrowserRouter } from "react-router-dom";
+
 import { store, persistor } from "./redux/store.js";
+import App from "./App.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <StrictMode>
