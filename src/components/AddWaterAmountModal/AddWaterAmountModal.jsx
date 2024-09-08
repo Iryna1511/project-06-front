39743676fdd3515c71aacf729780dc5d3da2a031
Valeriia Tyran <kuharets.v@gmail.com>
2024-09-08 13:05:05 +0300

    previos

--- src/components/AddWaterAmountModal/AddWaterAmountModal.jsx
+++ src/components/AddWaterAmountModal/AddWaterAmountModal.jsx
@@ -1,5 +1,5 @@
 import css from "./AddWaterAmountModal.module.css"
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 // import ErrorMsg from "../ErrorMsg/ErrorMsg";
 // import Loader from "../Loader/Loader.jsx"
 // import { NavLink } from "react-router-dom";
@@ -26,23 +26,115 @@ import TimeDropdown from "../TimeDropdown/TimeDropDown.jsx";
 //   },
 // };
 
+const initialState = { 
+    modal: {
+        isModalOpen: false,
+        waterAmount: 0,
+        enteredTime: '',
+        },
+}
+     
+export const incrementWaterAmount = () => ({
+    type: 'INCREMENT_WATER_AMOUNT',
+});
+
+export const decrementWaterAmount = () => ({
+    type: 'DECREMENT_WATER_AMOUNT',
+});
+
+export const setEnteredTime = (time) => ({
+    type: 'SET_TIME',
+    payload: time,
+});
+
+const reducer = (state = initialState, action) => {
+    switch (action.type) {
+        case 'INCREMENT_WATER_AMOUNT':
+            return {
+                ...state,
+                modal: {
+                    ...state.modal, waterAmount: state.modal.waterAmount + 1
+                },
+            };
+        case 'DECREMENT_WATER_AMOUNT':
+            return {
+                ...state,
+                modal: {
+                    ...state.modal, waterAmount: Math.max(0, state.modal.waterAmount - 1),
+                },
+            };
+        case 'SET_TIME':
+            return {
+                ...state,
+                modal: {
+                    ...state.modal,
+                    enteredTime: action.payload,
+                }
+            };
+        default: return state;
+    }
+};
+
+// const initialState = { waterLog: [] };
+
+// const waterSlice = createSlice({
+//     name: 'waterAmount',
+//     initialState,
+//     reducers: {
+//         addWaterLog: (state, action) => { state.waterLog.push(action.payload); },
+//         changeWaterlog: (state, action) => {state.waterLog}
+//     }
+// });
+
+// export const { addWaterLog, changeWaterlog } = waterSlice.actions;
+
+// export default waterSlice.reducer;
+
+// function waterAmount(
+//     const waterLog = useSelector(state => state.water.waterLog)
+// );
+
+// const dispatch = useDispatch();
+
+// const handleAddwater = (amount) => {
+//     dispatch(addWaterLog({ amount, time: newDate().getTime() }));
+// };
+
+
+// const rootReducer = (state = initialState, action) => {
+//   return state;
+// };
+// в store записуємо стан і редюсер
+
+
+
 
 export default function AddWaterAmountModal({ water: { amount, id } }) {
+    // const waterValue = useSelector(state => state.water.value);
+
     //  const isModalOpen = useSelector(selectIsModalOpen);
     // const isError = useSelector(selectError);
     // const isLoading = useSelector(selectLoading);
     const dispatch = useDispatch();
 
-    const handleAddWaterChanges = () => dispatch(addWaterAMount(id));
+    // const handleAddWater = () => dispatch(addWaterAmount(id));
+
+    const waterAmount = useSelector(state => state.modal.waterAmount);
+
+    const enterdTime = useSelector(state => state.modal.enteredTime);
+
+    const handleTimeChange = (event) => {
+        dispatch(setEnteredTime(event.target.value))
+    }
     
-    const handleClose = () => dispatch(closeModal());
+    const handleCloseModal = () => dispatch(toggleWaterModal());
 //   useEffect(() => {
 //     dispatch(fetchContacts())
 //   }, [dispatch])
 
   return (
       
-    <div className={css.backdrop} onClick={handleClose}>
+    <div className={css.backdrop} onClick={handleCloseModal}>
       <div className={css.modal}>
       {/* <Modal style={customStyles}> */}
       {/* {isError && <ErrorMsg/>} */}
@@ -50,14 +142,14 @@ export default function AddWaterAmountModal({ water: { amount, id } }) {
       {/* {isLoading && <Loader />} */}
       <div className={css.titlecontainer}>
         <h2 className={css.titletext}>Add water</h2>
-        <span className={css.closebtn} onClick={handleClose}><IoCloseOutline size="24" color="407BFF" /></span>
+        <span className={css.closebtn} onClick={handleCloseModal}><IoCloseOutline size="24" color="407BFF" /></span>
       </div>    
       <h3 className={css.subtitle}>Choose a value:</h3>
       <p className={css.signaturetext}>Amount of water:</p>
       <div className={css.waterInputcontainer}>
-        <button className={css.amountButton}type="button" onClick={minusWater}><HiOutlineMinusSmall size="24" color="407BFF" /></button>
-        <p className={css.amountWaterIncome}>{amount}</p>
-        <button className={css.amountButton} type="button" onClick={plusWater}><HiOutlinePlusSmall size="24" color="407BFF" /></button>
+        <button className={css.amountButton}type="button" onClick={() => dispatch(decrementWaterAmount())}><HiOutlineMinusSmall size="24" color="407BFF" /></button>
+        <p className={css.amountWaterIncome}>{waterAmount}</p>
+        <button className={css.amountButton} type="button" onClick={() => dispatch(incrementWaterAmount)}><HiOutlinePlusSmall size="24" color="407BFF" /></button>
       </div>
      
       <p className={css.signaturetext}>Recording time:</p>
@@ -65,7 +157,7 @@ export default function AddWaterAmountModal({ water: { amount, id } }) {
       <h3 className={css.subtitle}>Enter the value of the water used:</h3>
       <input className={css.waterAmount} type="text" onChange={handleWaterAmountChange}/>
       <div className={css.footerContainer}>
-        <p className={css.amountWaterIncomeFooter}>250 ml</p>
+        <p className={css.amountWaterIncomeFooter}>{waterAmount}</p>
         <button className={css.saveButton} type="button" onClick={handleAddWaterChanges}>Save</button>
       </div>
         {/* </Modal> */}
