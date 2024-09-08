c9c5290c1d0b76ab646ee56185b3d3b0f2ab4500
LytovchenkoOlenka <131744554+LytovchenkoOlenka@users.noreply.github.com>
2024-09-08 21:41:45 +0200

    fixed_css_home

--- src/components/WaterRatioPanel/WaterRatioPanel.jsx
+++ src/components/WaterRatioPanel/WaterRatioPanel.jsx
@@ -1,7 +1,6 @@
 import css from "./WaterRatioPanel.module.css";
 import AddWaterButton from "../AddWaterButton/AddWaterButton";
 
-
 export default function WaterRatioPanel() {
   // const { drankToday } = useSelector();
   const drankTodayValue = 15;
@@ -9,37 +8,34 @@ export default function WaterRatioPanel() {
   return (
     <>
       <div className={css.marginSection}>
-      <h2 className={css.title}>Today</h2>
-
-      <div className={css.wrapper}>
-        <div className={css.progressContainer}>
-          <div className={css.progressBar}>
-            <div
-              className={css.progressCompleted}
-              style={{ width: `${drankTodayValue}%` }}
-            >
-              <div className={css.percentageMarker}>
-                <p className={css.percentageText}>{drankTodayValue}%</p>
+        <h2 className={css.title}>Today</h2>
+        <div className={css.wrapper}>
+          <div className={css.progressContainer}>
+            <div className={css.progressBar}>
+              <div
+                className={css.progressCompleted}
+                style={{ width: `${drankTodayValue}%` }}
+              >
+                <div className={css.percentageMarker}>
+                  <p className={css.percentageText}>{drankTodayValue}%</p>
+                </div>
+              </div>
+            </div>
+            <div className={css.scaleContainer}>
+              <div className={css.scaleTick}>
+                <span className={css.scaleText}>0%</span>
+              </div>
+              <div className={css.scaleTick}>
+                <span className={css.scaleTextBold}>50%</span>
+              </div>
+              <div className={css.scaleTick}>
+                <span className={css.scaleText}>100%</span>
               </div>
             </div>
           </div>
-          <div className={css.scaleContainer}>
-            <div className={css.scaleTick}>
-              <span className={css.scaleText}>0%</span>
-            </div>
-            <div className={css.scaleTick}>
-              <span className={css.scaleTextBold}>50%</span>
-            </div>
-            <div className={css.scaleTick}>
-              <span className={css.scaleText}>100%</span>
-            </div>
-          </div>
+          <AddWaterButton />
         </div>
-       <AddWaterButton />
-        </div>
-
       </div>
-
     </>
   );
 }
