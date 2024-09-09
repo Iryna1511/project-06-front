6725ccc78a5d4e15835ac9e2451506e76e857ec4
YakymenkoOleksandr <93155369+YakymenkoOleksandr@users.noreply.github.com>
2024-09-09 22:31:03 +0300

    addcomit1

--- src/components/UserLogoModal/UserLogoModal.jsx
+++ src/components/UserLogoModal/UserLogoModal.jsx
@@ -1,3 +1,4 @@
+import { useEffect, useRef } from "react";
 import { PiGear } from "react-icons/pi";
 import { TbLogout } from "react-icons/tb";
 import styles from "./UserLogoModal.module.css";
@@ -7,12 +8,26 @@ export default function UserLogoModal({
   openLogoutModal,
   closeUserModal,
 }) {
+  const modalRef = useRef(null); // Створюємо реф для модального вікна
+
+  useEffect(() => {
+    const handleClickOutside = (event) => {
+      if (modalRef.current && !modalRef.current.contains(event.target)) {
+        closeUserModal(); // Закриваємо модальне вікно при кліку поза ним
+      }
+    };
+
+    // Додаємо обробник події
+    document.addEventListener("mousedown", handleClickOutside);
+
+    // Очищаємо обробник при демонтунні компонента
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  }, [closeUserModal]);
+
   return (
-    <div
-      className={styles.modalContent}
-      onClick={() => {
-      }}
-    >
+    <div ref={modalRef} className={styles.modalContent}>
       <div className={styles.modalItem} onClick={openSettingModal}>
         <PiGear className={styles.PiGear} />
         <span className={styles.text}>Settings</span>
