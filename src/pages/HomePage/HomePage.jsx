e91894b85b32da1fa60e563cc9fdca8f21ad67d0
Iryna1511 <148438865+Iryna1511@users.noreply.github.com>
2024-09-09 17:42:18 +0300

    відновила хоум

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,41 +1,42 @@
 import css from "./HomePage.module.css";
 import TodayWaterList from "../../components/TodayWaterList/TodayWaterList";
-// import { useSelector } from "react-redux";
-// import {
-//   // selectIsAddWaterMdOpen,
-//   // selectIsTodayListMdOpen,
-//   // selectIsDeleteEntryOpen,
-// } from "../../redux/waterIrina/irinaSelectors"; // селектор стану
+import { useSelector } from "react-redux";
+import {
+  // selectIsAddWaterMdOpen,
+  // selectIsTodayListMdOpen,
+  selectIsDeleteEntryOpen,
+} from "../../redux/water/waterSelectors"; // селектор стану
 // import AddWaterAmountModal from "../../components/AddWaterAmountModal/AddWaterAmountModal";
 // import TodayListModal from "../../components/TodayListModal/TodayListModal";
-// import DeleteEntry from "../../components/DeleteEntry/DeleteEntry";
-// import DailyNorma from "../../components/DailyNorma/DailyNorma";
-// import WaterRatioPanel from "../../components/WaterRatioPanel/WaterRatioPanel";
+import DeleteEntry from "../../components/DeleteEntry/DeleteEntry";
+import DailyNorma from "../../components/DailyNorma/DailyNorma";
+import WaterRatioPanel from "../../components/WaterRatioPanel/WaterRatioPanel";
 
-// import MonthStatsTable from "../../components/MonthStatsTable/MonthStatsTable";
+import MonthStatsTable from "../../components/MonthStatsTable/MonthStatsTable";
 
 export default function HomePage() {
   // const isAddModalOpen = useSelector(selectIsAddWaterMdOpen);
   // const isEditModalOpen = useSelector(selectIsTodayListMdOpen);
-  // const isDeleteModalOpen = useSelector(selectIsDeleteEntryOpen);
+  const isDeleteModalOpen = useSelector(selectIsDeleteEntryOpen);
 
   return (
     <>
       <div className={css.generalContainer}>
         <div className={css.infoContainer}>
           <div className={css.containerWater}>
-            {/* <DailyNorma /> */}
-            {/* <WaterRatioPanel /> */}
+            <DailyNorma />
+            <WaterRatioPanel />
           </div>
         </div>
+
         <div className={css.tables}>
           <TodayWaterList />
-          {/* <MonthStatsTable /> */}
+          <MonthStatsTable />
         </div>
       </div>
       {/* {isAddModalOpen && <AddWaterAmountModal />}
       {isEditModalOpen && <TodayListModal />} */}
-      {/* {isDeleteModalOpen && <DeleteEntry />} */}
+      {isDeleteModalOpen && <DeleteEntry />}
     </>
   );
 }
