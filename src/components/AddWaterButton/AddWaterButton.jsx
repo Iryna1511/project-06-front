39d2a6cb59136f7834f30ce58c4afb1ae2627b57
Iryna1511 <148438865+Iryna1511@users.noreply.github.com>
2024-09-10 10:31:53 +0300

    open Add water Modal

--- src/components/AddWaterButton/AddWaterButton.jsx
+++ src/components/AddWaterButton/AddWaterButton.jsx
@@ -1,21 +1,25 @@
 //import { useState } from "react";
 import css from "./AddWaterButton.module.css";
 import Icons from "../Icons/Iсons";
+import { toggleAddWaterModal } from "../../redux/mainWater/slice";
+import { useDispatch } from "react-redux";
 
 export default function AddWaterButton() {
- // const [isModalOpen, setIsModalOpen] = useState(false);
+  const dispatch = useDispatch();
+  const openModal = () => dispatch(toggleAddWaterModal());
+  // const [isModalOpen, setIsModalOpen] = useState(false);
 
- // const closeModal = () => {
+  // const closeModal = () => {
   //  setIsModalOpen(false);
   //};
 
   //const openModal = () => {
   //  setIsModalOpen(true);
- // };
+  // };
 
   return (
     <>
-      <button className={css.btn} type="button" /*onClick={openModal}*/>
+      <button className={css.btn} type="button" onClick={openModal}>
         <Icons id="plus-inside" width="24" height="24" className="icon-white" />
         Add Water
       </button>
